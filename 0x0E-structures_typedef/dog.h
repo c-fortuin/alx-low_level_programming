a354efeb2f2083877c2cac053ce809efef36e275
c-fortuin <chrislifortuin@gmail.com>
2023-06-19 20:18:20 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- Dogs info
+ * @name: Dogs name
+ * @age: Dogs age
+ * @owner: Dogs owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *strcpy(char *dest, char *src);
+
+#endif
