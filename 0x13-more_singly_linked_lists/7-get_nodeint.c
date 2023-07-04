dd75cd1615bfcf16ad7aec7e8e2e742eaa85c671
Chrisli <chrislifortuin@gmail.com>
2023-07-03 17:10:06 -0700

    0x13. C - More singly linked lists mandatory

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index- Returns node at certain index
+ * @head: First node
+ * @index: index of the node to return
+ * Return: Power to the node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = head;
+
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	return (temp ? temp : NULL);
+}
